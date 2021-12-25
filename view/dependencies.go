f31d84816a21ac70918771af3566acecf930e7ce
Gregory Siems <gsiems@gmail.com>
2021-12-25 09:40:52 -0600

    Fix sortDependencies function

--- view/dependencies.go
+++ view/dependencies.go
@@ -1,15 +1,43 @@
 package view
 
 import (
-	//"html/template"
-	//"os"
 	"sort"
+	"strings"
 
 	m "github.com/gsiems/db-dictionary/model"
 )
 
-func sortDependencies(dependencies []m.Dependency) {
-	sort.Slice(dependencies, func(i, j int) bool {
-		return dependencies[i].ObjectSchema < dependencies[j].ObjectSchema && dependencies[i].ObjectName < dependencies[j].ObjectName && dependencies[i].DepObjectSchema < dependencies[j].DepObjectSchema && dependencies[i].DepObjectName < dependencies[j].DepObjectName
+func sortDependencies(x []m.Dependency) {
+	sort.Slice(x, func(i, j int) bool {
+
+		switch strings.Compare(x[i].ObjectSchema, x[j].ObjectSchema) {
+		case -1:
+			return true
+		case 1:
+			return false
+		}
+
+		switch strings.Compare(x[i].ObjectName, x[j].ObjectName) {
+		case -1:
+			return true
+		case 1:
+			return false
+		}
+
+		switch strings.Compare(x[i].DepObjectSchema, x[j].DepObjectSchema) {
+		case -1:
+			return true
+		case 1:
+			return false
+		}
+
+		switch strings.Compare(x[i].ObjectName, x[j].ObjectName) {
+		case -1:
+			return true
+		case 1:
+			return false
+		}
+
+		return x[i].DepObjectName > x[j].DepObjectName
 	})
 }
